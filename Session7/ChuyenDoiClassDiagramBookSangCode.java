c02e9ec1f1924d69941c387397482a3381a56c6a
NguyenDuyThang <thangduy2501@gmail.com>
2026-01-21 18:48:37 +0700

    hoan thanh

---
+++ Session7/ChuyenDoiClassDiagramBookSangCode.java
@@ -0,0 +1,11 @@
+public class Book {
+    String title;
+    String author;
+    double price;
+
+     public void printInfo() {
+         System.out.println("Title: " + title);
+         System.out.println("Author: " + author);
+         System.out.println("Price: " + price);
+    }
+}
